8b4e5b3123a30acce4e683869e84174ba5d65387
Tobias Bohwalli <hi@futhr.io>
2013-05-16 15:57:29 +0200

    Simplified spec and filters for simplecov.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,8 @@ if ENV["COVERAGE"]
     Coveralls::SimpleCov::Formatter
   ]
   SimpleCov.start do
+    add_filter '/spec/'
     add_group 'Controllers', 'app/controllers'
-    add_group 'Helpers', 'app/helpers'
-    add_group 'Mailers', 'app/mailers'
     add_group 'Models', 'app/models'
     add_group 'Overrides', 'app/overrides'
     add_group 'Views', 'app/views'
