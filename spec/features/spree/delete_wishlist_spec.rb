1ae3057c8e66715384e988cfa78f30b91f1389cb
Tobias Bohwalli <hi@futhr.io>
2013-07-23 17:54:20 +0200

    Fix syntax and consistency.

--- spec/features/spree/delete_wishlist_spec.rb
+++ spec/features/spree/delete_wishlist_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper'
 
-feature "Delete Wishlist", js: true do
-  let(:wishlist){ create(:wishlist) }
-  let(:user){ wishlist.user }
+feature "Delete Wishlist" do
+  given(:wishlist) { create(:wishlist) }
+  given(:user) { wishlist.user }
 
   scenario "delete a user's wishlist" do
-    visit '/login'
-    within('#new_spree_user') do
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
+    visit "/login"
+    within "#new_spree_user" do
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
     end
-    click_button 'Login'
+    click_button "Login"
 
-    visit '/account'
+    visit "/account"
     click_link "#{wishlist.name}"
-    click_link 'Edit wishlist'
-    click_link 'Delete wishlist'
-    page.driver.accept_js_confirms!
+    click_link "Edit wishlist"
+    click_link "Delete wishlist"
     expect(page).not_to have_content("#{wishlist.name}")
   end
 end
--- spec/features/spree/edit_wishlist_spec.rb
+++ spec/features/spree/edit_wishlist_spec.rb
@@ -1,75 +1,75 @@
 require 'spec_helper'
 
-feature "Edit Wishlist", js: true do
-  let(:wishlist){ create(:wishlist) }
-  let(:user){ wishlist.user }
+feature "Edit Wishlist" do
+  given(:wishlist) { create(:wishlist) }
+  given(:user) { wishlist.user }
 
-  before :each do
-    visit '/login'
-    within('#new_spree_user') do
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
+  background do
+    visit "/login"
+    within "#new_spree_user" do
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
     end
-    click_button 'Login'
+    click_button "Login"
   end
 
   scenario "edit a wishlist's name" do
-    visit '/account'
+    visit "/account"
     click_link "#{wishlist.name}"
-    click_link 'Edit wishlist'
-    fill_in 'Name', with: 'A New Wishlist Name'
-    click_button 'Update'
-    expect(page).to have_content('A New Wishlist Name')
+    click_link "Edit wishlist"
+    fill_in "Name", with: "A New Wishlist Name"
+    click_button "Update"
+    expect(page).to have_content("A New Wishlist Name")
   end
 
-  describe "edit a wishlist's is_private" do
+  context "edit a wishlist's is_private" do
     scenario "set wishlist from private to public" do
       wishlist.is_private = true
       wishlist.save
       # wishlist.update(is_private: true) throws up a NoMethodError
-      visit '/account'
+      visit "/account"
       click_link "#{wishlist.name}"
-      click_link 'Edit wishlist'
-      uncheck('is private')
-      click_button 'Update'
-      expect(page).to have_checked_field('wishlist_is_private_false')
+      click_link "Edit wishlist"
+      uncheck "is private"
+      click_button "Update"
+      expect(page).to have_checked_field("wishlist_is_private_false")
     end
 
     scenario "set wishlist from public to private" do
       wishlist.is_private = false
       wishlist.save
-      visit '/account'
+      visit "/account"
       click_link "#{wishlist.name}"
-      click_link 'Edit wishlist'
-      check('is private')
-      click_button 'Update'
-      expect(page).to have_checked_field('wishlist_is_private_true')
+      click_link "Edit wishlist"
+      check "is private"
+      click_button "Update"
+      expect(page).to have_checked_field("wishlist_is_private_true")
     end
   end
 
-  describe "edit a wishlist's is_default" do
+  context "edit a wishlist's is_default" do
     scenario "set wishlist from default to non-default" do
       wishlist.is_default = true
       wishlist.save
-      visit '/account'
+      visit "/account"
       click_link "#{wishlist.name}"
-      click_link 'Edit wishlist'
-      uncheck('is default')
-      click_button 'Update'
-      click_link 'Edit wishlist'
-      expect(page).to have_unchecked_field('is default')
+      click_link "Edit wishlist"
+      uncheck "is default"
+      click_button "Update"
+      click_link "Edit wishlist"
+      expect(page).to have_unchecked_field("is default")
     end
 
     scenario "set wishlist from non-default to default" do
       wishlist.is_default = false
       wishlist.save
-      visit '/account'
+      visit "/account"
       click_link "#{wishlist.name}"
-      click_link 'Edit wishlist'
-      check('is default')
-      click_button 'Update'
-      click_link 'Edit wishlist'
-      expect(page).to have_checked_field('is default')
+      click_link "Edit wishlist"
+      check "is default"
+      click_button "Update"
+      click_link "Edit wishlist"
+      expect(page).to have_checked_field("is default")
     end
   end
 end
