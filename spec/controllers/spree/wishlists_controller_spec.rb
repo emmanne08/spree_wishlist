d91a3763abf5dcc102401e3597c0f0818e025ded
Emmanuel Cagadas <ec.secondary@gmail.com>
2013-07-30 09:42:17 +0800

    Added functional tests on wishlists controller

--- spec/controllers/spree/wishlists_controller_spec.rb
+++ spec/controllers/spree/wishlists_controller_spec.rb
@@ -4,8 +4,8 @@ describe Spree::WishlistsController do
   stub_authorization!
 
   let!(:user) { create(:user) }
-  let!(:wishlist) { create(:wishlist) }
-  let (:valid_attributes) { attributes_for(:wishlist) }
+  let!(:wishlist) { create(:wishlist, user: user) }
+  let(:valid_attributes) { attributes_for(:wishlist) }
 
   before { controller.stub spree_current_user: user }
 
@@ -22,9 +22,8 @@ describe Spree::WishlistsController do
 
   context "#index" do
     it "assigns all wishlists as @wishlists" do
-      pending "empty array? probably need to fix factory so its populated correctly"
       spree_get :index
-      assigns(:wishlists).should eq [wishlist]
+      assigns(:wishlists).should eq user.wishlists
     end
   end
 
@@ -36,31 +35,49 @@ describe Spree::WishlistsController do
   end
 
   context "#update" do
-    context "with valid params" do
-      it "updates the requested wishlist" do
-        Spree::Wishlist.any_instance.should_receive(:update_attributes).with({ 'these' => 'params' })
-        spree_put :update, id: wishlist, wishlist: { 'these' => 'params' }
+    it "updates the requested wishlist with some wishlist parameters" do
+      Spree::Wishlist.any_instance.should_receive(:update_attributes).with({ 'these' => 'params' })
+      spree_put :update, id: wishlist, wishlist: { 'these' => 'params' }
+    end
+
+    it "assigns @wishlist" do
+      spree_put :update, id: wishlist
+      assigns(:wishlist).should eq wishlist
+    end
+
+    context "when the wishlist updates successfully" do
+      before :each do
+        spree_put :update, id: wishlist, wishlist: valid_attributes
       end
 
-      it "assigns the requested wishlist as @wishlist" do
-        spree_put :update, id: wishlist, wishlist: valid_attributes
-        assigns(:wishlist).should eq wishlist
-      end
-
-      it "redirects to the wishlist" do
-        spree_put :update, id: wishlist, wishlist: valid_attributes
+      it "redirects to the updated wishlist" do
         response.should redirect_to wishlist
+      end
+
+      it "sets the attributes of @wishlist according to valid_attributes" do
+        valid_attributes.each do |attr_name, value|
+          assigns(:wishlist).send(attr_name).should eq value
+        end
       end
     end
 
-    context "with invalid params" do
-      before do
-        Spree::Wishlist.any_instance.stub(:save).and_return(false)
+    context "when the wishlist fails to update" do
+      before :each do
         spree_put :update, id: wishlist, wishlist: {}
       end
-      it { assigns(:wishlist).should eq wishlist }
-      # expecting <"edit"> but rendering with <"">
-      pending { response.should render_template :edit }
+
+      it "redirects to the non-updated wishlist" do
+        response.should redirect_to wishlist
+      end
+
+      it "sets the attributes of @wishlist according to wishlist's initial attributes" do
+        exclude = ["created_at", "updated_at"]
+        Spree::Wishlist.attribute_names.
+          reject { |attr_name| exclude.include?(attr_name) }.
+          each do |attr_name|
+            assigns(:wishlist).send(attr_name).should eq wishlist.send(attr_name)
+          end
+      end
     end
   end
 
@@ -71,42 +88,75 @@ describe Spree::WishlistsController do
     end
   end
 
-  pending "#default" do
-    it "assigns the requested wishlist as @wishlist" do
+  context "#default" do
+    it "retrieves the default wishlist of the current user" do
+      Spree::User.any_instance.should_receive(:wishlist)
       spree_get :default
-      assigns(:wishlist).should eq wishlist
     end
-    it { response.should render_template :show }
+
+    it "assigns the default wishlist as @wishlist" do
+      spree_get :default
+      assigns(:wishlist).should eq user.wishlist
+    end
+
+    it "renders the wishlists/show template" do
+      spree_get :default
+      response.should render_template "spree/wishlists/show"
+    end
   end
 
   context "#create" do
-    context "with valid params" do
-      it "creates a new Spree::Wishlist" do
+    it "creates a new wishlist with some wishlist parameters" do
+      Spree::Wishlist.should_receive(:new).
+        with({ 'these' => 'params' }).
+        and_return(mock_model(Spree::Wishlist).as_null_object)
+      spree_post :create, wishlist: { 'these' => 'params' }
+    end
+
+    it "assigns @wishlist" do
+      spree_post :create
+      assigns(:wishlist).should be_an_instance_of(Spree::Wishlist)
+    end
+
+     it "sets the current user as the user of @wishlist" do
+       spree_post :create
+       assigns(:wishlist).user.should eq user
+     end
+
+    context "when the wishlist saves successfully" do
+      before :each do
+        spree_post :create, wishlist: valid_attributes
+      end
+
+      it "saves the new wishlist" do
         expect {
           spree_post :create, wishlist: valid_attributes
         }.to change(Spree::Wishlist, :count).by(1)
       end
 
-      it "assigns a newly created wishlist as @wishlist" do
-        spree_post :create, wishlist: valid_attributes
-        assigns(:wishlist).should be_a Spree::Wishlist
-        assigns(:wishlist).should be_persisted
+      it "redirects to the newly created wishlist" do
+        response.should redirect_to user.wishlists.last
       end
 
-      it "redirects to the created wishlist" do
-        spree_post :create, wishlist: valid_attributes
-        response.should redirect_to Spree::Wishlist.last
+      it "sets the attributes of @wishlist according to valid_attributes" do
+        valid_attributes.each do |attr_name, value|
+          assigns(:wishlist).send(attr_name).should eq value
+        end
       end
     end
 
-    context "with invalid params" do
-      before do
-        Spree::Wishlist.any_instance.stub(:save).and_return(false)
+    context "when the wishlist fails to save" do
+      before :each do
         spree_post :create, wishlist: {}
       end
-      it { assigns(:wishlist).should be_a_new Spree::Wishlist }
-      # expecting <"new"> but rendering with <"">
-      pending { response.should render_template :new }
+
+      it "renders the wishlists/new template" do
+        response.should render_template "spree/wishlists/new"
+      end
+
+      it "doesn't save the new wishlist" do
+        assigns(:wishlist).should be_a_new Spree::Wishlist
+      end
     end
   end
 
@@ -117,15 +167,9 @@ describe Spree::WishlistsController do
       }.to change(Spree::Wishlist, :count).by(-1)
     end
 
-    it "redirects to the wishlists list" do
-      pending "undefined local variable or method `wishlists_url'"
+    it "redirects to the user's account page" do
       spree_delete :destroy, id: wishlist
-      response.should redirect_to wishlists_url
-    end
-
-    it "requires the :id parameter" do
-      pending "expected ActionController::RoutingError, got #<NoMethodError: undefined method `destroy' for nil:NilClass> with backtrace:"
-      expect { spree_delete :destroy }.to raise_error(ActionController::RoutingError)
+      response.should redirect_to spree.account_path
     end
   end
 end
